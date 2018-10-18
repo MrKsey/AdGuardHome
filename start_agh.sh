a056c637763775243ac01664ced5395377796628
ksey <ksey3000@gmail.com>
2018-10-18 14:47:48 +0300

    Update start_agh.sh

--- start_agh.sh
+++ start_agh.sh
@@ -4,4 +4,6 @@ if [ ! -e /etc/AdGuardHome/AdGuardHome.yaml ]; then
   cp /AdGuardHome.yaml /etc/AdGuardHome/AdGuardHome.yaml
 fi
 
+chmod 666 /etc/AdGuardHome/AdGuardHome.yaml
+
 /opt/AdGuardHome/AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml
