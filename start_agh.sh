ddcc38b0f3633d6296cd0fc42d1cdf2b4a64ebe9
ksey <ksey3000@gmail.com>
2019-02-26 03:38:21 +0300

    Delete start_agh.sh

--- start_agh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e /opt/AdGuardHome/config/AdGuardHome.yaml ]; then
-  cp /AdGuardHome.yaml /opt/AdGuardHome/config/AdGuardHome.yaml
-fi
-
-chmod 666 /opt/AdGuardHome/config/AdGuardHome.yaml
-
-/opt/AdGuardHome/AdGuardHome -c /config/AdGuardHome.yaml
